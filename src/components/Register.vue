953c5db09729081852888f75d0dbba2ab0e55aa6
Steve Gazzo <stevegazzojr@gmail.com>
2024-05-04 23:29:24 -0400

    cleaned up registration page a bit

---
+++ src/components/Register.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="wrapper">
+    <h2>Register</h2>
+    <br />
+    <form>
+      <label for="email">Email</label>
+      <br />
+      <input class="text" id="email" name="email" v-model="email" />
+      <label for="phone">Phone</label>
+      <br />
+      <input class="text" id="phone" name="phone" v-model="phone" />
+      <br />
+      <label for="contactPreference">Contact preference</label>
+      <br />
+      <select
+        class="text"
+        name="contactPreference"
+        id="contactPreference"
+        v-model="contactPreference"
+      >
+        <option selected value="EMAIL">Email</option>
+        <option value="SMS">SMS</option>
+      </select>
+    </form>
+    <br />
+    <button
+      class="submit-button"
+      @click.prevent="submitUser(email, phone, contactPreference)"
+    >
+      Submit
+    </button>
+    <br />
+  </div>
+</template>
+<script>
+import axios from "axios";
+import { useAuthStore } from "./stores/authStore";
+
+export default {
+  name: "EventFetcher",
+  data() {
+    return {
+      authStore: useAuthStore(),
+      contactPreference: "EMAIL",
+    };
+  },
+  methods: {
+    async submitUser(email, phone, contactPreference) {
+      await axios.post("http://localhost:8081/user", {
+        email: email,
+        phone: phone,
+        preference: contactPreference,
+      });
+    },
+  },
+};
+</script>
